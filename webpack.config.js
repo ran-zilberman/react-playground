2b07a8af419b17f99fb6cbbe05edd4f1a6c06366
Ran_Zilberman <ranz@wix.com>
2016-04-25 04:21:46 +0300

    adding react + babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
+var APP_COMPONENT_LOCATION = path.resolve(__dirname, 'src/components/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: {
+    javascript: APP_COMPONENT_LOCATION + '/index.jsx',
+    html: SRC_DIR + "/index.html"
+  },
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
@@ -14,8 +18,12 @@ var config = {
     loaders : [
       {
         test : /\.jsx?/,
-        include : APP_DIR,
+        include : SRC_DIR,
         loader : 'babel'
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
       }
     ]
   }
